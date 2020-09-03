d89cc769f2470ad0486f513e7e47d5a6a45dc401
NiansaSA <niansa_sixty_27rpl@student.smktelkom-mlg.sch.id>
2020-09-03 15:26:19 +0700

    Vue Router

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <ul class="nav" style="background-color : #e3f2fd;">
+      <li class="nav-item"><router-link class="nav-link" to="/">HOME</router-link></li>
+      <li class="nav-item"><router-link class="nav-link" to="/about">ABOUT</router-link></li>
+      <li class="nav-item"><router-link class="nav-link" to="/contact">CONTACT</router-link></li>
+    </ul>
+
+    <router-view></router-view>
+  
+  </div>
+</template>
+
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
